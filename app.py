93b33bd45b1d498f90905012575a23c468d778df
Kunal Mehta <legoktm@member.fsf.org>
2018-04-25 08:39:33 -0700

    hmm

--- app.py
+++ app.py
@@ -204,8 +204,7 @@ def search(term):
     for entry in parsed['entries']:
         if entry['link'] in known_stuff:
             # play it
-            return question(_play(known_stuff[entry['link']]))\
-                .simple_card('California Speaks', 'Playing results for "%s"' % term)
+            return audio().play(s3_url(known_stuff[entry['link']]))
     text = 'Sorry, unable to find anything related to "%s". Try searching again?' % term
     return question(text).simple_card('California Speaks', text)
 
